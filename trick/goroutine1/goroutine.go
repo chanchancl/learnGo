f37dd0caf3bbaa8a9b9213444a8565669cf0cc60
Ccl <540046768@qq.com>
2019-01-08 15:54:49 +0800

    water goroutine

---
+++ trick/goroutine1/goroutine.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go func() { fmt.Println(i) }()
+	}
+
+	time.Sleep(time.Second)
+}
---
+++ trick/goroutine2/goroutine.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go func(i int) { fmt.Println(i) }(i)
+	}
+
+	time.Sleep(time.Second)
+}

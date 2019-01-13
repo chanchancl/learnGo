47cd11bea83a3356857a70f62790440943c61996
Ccl <540046768@qq.com>
2019-01-13 23:17:15 +0800

    water

--- test/test.go
+++ test/test.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var keyBuf []byte
-	kaut := "0001020304050607080910ffaa"
+	kaut := "0001020304050607080910ffaa00"
 	fmt.Sscanf(kaut, "%x", &keyBuf)
 	fmt.Printf("%#v\n", keyBuf)
 }

58267172f95334008a3e003e124a4f9ee2d7247a
Ccl <540046768@qq.com>
2019-01-15 17:29:09 +0800

    water forget fmt

--- pm/vec/main.go
+++ pm/vec/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -20,7 +21,7 @@ func main() {
 	vec.WithLabelValues("success").Add(0)
 	vec.WithLabelValues("failure").Add(0)
 	vec.WithLabelValues("total").Inc()
-	
+
 	http.Handle("/metrics", prometheus.Handler())
 	fmt.Println("Listening!")
 	log.Fatal(http.ListenAndServe(":8080", nil))

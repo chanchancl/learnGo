0328442904a1e76eabbdb7e6fabe7699b8edb5c1
Ccl <540046768@qq.com>
2019-01-23 17:50:37 +0800

    water fetch https and Stderr

--- fetch/main.go
+++ fetch/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	for _, url := range os.Args[1:] {
 
-		if !strings.HasPrefix(url, "http://") {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
 			url = "http://" + url
 		}
 		resp, err := http.Get(url)
@@ -21,7 +21,7 @@ func main() {
 		}
 		defer resp.Body.Close()
 
-		if _, err := io.Copy(os.Stderr, resp.Body); err != nil {
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}

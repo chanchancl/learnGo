8d1b8c06861540dbfad7798fb4d08bc48fef9b62
Ccl <540046768@qq.com>
2019-11-28 13:40:56 +0800

    water

--- basic/url/main.go
+++ basic/url/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-
-	URL := "https://www.hostname.com:1234/iam/path/"
+	//URL := "https://www.hostname.com:1234/iam/path/"
+	URL := "https://2001:3CA1:010F:001A:121B:0000:0000:0010:1234/iam/path/"
 	u, _ := url.Parse(URL)
 
 	fmt.Println(u.Scheme)

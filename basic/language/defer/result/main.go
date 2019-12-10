215fa13293009b7979a5d0ed70b29f4773169a37
Ccl <540046768@qq.com>
2019-12-10 17:37:39 +0800

    water defer

---
+++ basic/language/defer/result/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func double(x int) (result int) {
+	defer func() { fmt.Printf("double(%d) = %d\n", x, result) }()
+	return x + x
+}
+
+func triple(x int) (result int) {
+	defer func() { result += x }()
+	return double(x)
+}
+
+func main() {
+	fmt.Println(triple(4))
+}

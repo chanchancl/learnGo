76cafda963889270be53d4707299c6d577ec59bf
Ccl <540046768@qq.com>
2019-12-17 18:09:14 +0800

    water fmt

--- basic/tools/fmt/main.go
+++ basic/tools/fmt/main.go
@@ -2,9 +2,16 @@ package main
 
 import "fmt"
 
+type std struct {
+	a, b int
+	c    string
+}
+
 func main() {
 	fmt.Printf("%#v\n", 1.0)
 	var f float64
 	f = 356848.0
 	fmt.Printf("%#f\n", f)
+
+	fmt.Printf("%#v\n", std{})
 }

bbb39c7fc7e0f24e4469404c4aa6c3c67c8be025
Ccl <540046768@qq.com>
2020-02-10 09:12:54 +0100

    water readdir

---
+++ basic/language/io/readDir/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	pathpkg "path"
+	"strings"
+)
+
+func main() {
+	read(os.Args[1], 0)
+}
+
+func read(path string, indent int) {
+	infos, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println("Read %s failed, %s\n", path, err.Error())
+		return
+	}
+	for _, info := range infos {
+		if info.IsDir() {
+			fmt.Printf("%s%s\\\n", strings.Repeat(" ", indent), info.Name())
+			read(pathpkg.Join(path, info.Name()), indent+2)
+		} else {
+			fmt.Printf("%s%s\n", strings.Repeat(" ", indent), info.Name())
+		}
+	}
+}

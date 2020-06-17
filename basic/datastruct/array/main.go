095d5b5a2d3494f44b6fb8a27c65eb52361327bd
Ccl <540046768@qq.com>
2020-06-17 09:12:25 +0200

    water array, slice and recover

--- basic/datastruct/array/main.go
+++ basic/datastruct/array/main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	vector := []string{}
-	vector[0] = "1"
-	// error!!!
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer func() {
+			defer wg.Done()
+			if r := recover(); r != nil {
+				fmt.Println("Recovered in f", r)
+			}
+		}()
+
+		// slice
+		vector1 := []string{}
+		vector1[0] = "1"
+		// error!!!
+	}()
+
+	// array 5
+	vector2 := [5]string{}
+	vector2[0] = "1"
+
+	fmt.Println(vector2[0])
+
+	wg.Wait()
 }

0213b41aae8fbae309cdc228fc4f52acbda62a2f
Ccl <540046768@qq.com>
2020-07-13 10:08:51 +0200

    water mutex with block(don't use)

--- basic/tools/lock/mutexWithBlock/main.go
+++ basic/tools/lock/mutexWithBlock/main.go
@@ -15,6 +15,9 @@ func main() {
 		defer mutex.Unlock()
 		fmt.Println("Lock")
 	}
+	mutex.Lock()
 	fmt.Println("Unlock")
-
+	// This prove that block don't trigger the defer function
+	// mutex is locked during the function
+	// So don't use this
 }

63658b437ce46b2f0f5b75a6221e66a3f63aa74f
Ccl <540046768@qq.com>
2020-07-27 10:57:21 +0200

    water strings.Builder

--- basic/string/stringbuilder/main.go
+++ basic/string/stringbuilder/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	a := strings.Builder{}
 	a.WriteString("qwe")
 
-	// copy
+	// copy, Don't copy strings.Builder
 	b := a
 	b.WriteString("abc")
+	fmt.Println(a.String())
+	fmt.Println(b.String())
 }

c24b85bb57adacf5ae2d37b150401df67bb77eec
Ccl <540046768@qq.com>
2020-08-27 09:36:00 +0200

    water urlWithoutQuery

--- basic/network/url/main.go
+++ basic/network/url/main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 func main() {
 	//URL := "https://www.hostname.com:1234/iam/path/"
-	URL := "https://2001:3CA1:010F:001A:121B:0000:0000:0010:1234/iam/path/"
+	URL := "https://user:password@2001:3CA1:010F:001A:121B:0000:0000:0010:1234/iam/path/"
 	u, _ := url.Parse(URL)
 
 	fmt.Println(u.Scheme)
-	fmt.Println(u.User)
+	user := u.User
+	fmt.Println(user.Username())
+	fmt.Println(user.Password())
 	fmt.Println(u.Path)
 
 	fmt.Println(u.Host)
 
 	fmt.Println(u.Hostname())
 	fmt.Println(u.Port())
-
+	fmt.Println(u.RequestURI())
+	fmt.Println(u.String()[:strings.Index(u.String(), "?")])
 }

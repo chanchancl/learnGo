a99cf253f6aee9e9811c234fc63de5e93544a0e7
Ccl <ccl.love.miku@gmail.com>
2021-12-08 14:42:40 +0800

    Water URL

--- basic/network/url/main.go
+++ basic/network/url/main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"fmt"
 	"net/url"
-	"strings"
 )
 
 func main() {
 	//URL := "https://www.hostname.com:1234/iam/path/"
 	URL := "https://user:password@2001:3CA1:010F:001A:121B:0000:0000:0010:1234/iam/path/abc?a=10&b=20&c=100&d=你好"
 	u, _ := url.Parse(URL)
-
-	fmt.Println(u.Scheme)
 	user := u.User
-	fmt.Println(user.Username())
-	fmt.Println(user.Password())
-	fmt.Println(u.Path)
 
-	fmt.Println(u.Host)
-
-	fmt.Println(u.Hostname())
-	fmt.Println(u.Port())
-	fmt.Println(u.RequestURI())
-	fmt.Println(u.String()[:strings.Index(u.String(), "?")])
+	println(URL)
+	fmt.Printf("Schema     : %v\n", u.Scheme)
+	fmt.Printf("Username   : %v\n", user.Username())
+	password, _ := user.Password()
+	fmt.Printf("Password   : %v\n", password)
+	fmt.Printf("Host       : %v\n", u.Host)
+	fmt.Printf("Hostname   : %v\n", u.Hostname())
+	fmt.Printf("Port       : %v\n", u.Port())
+	fmt.Printf("Path       : %v\n", u.Path)
+	fmt.Printf("RequestURI : %v\n", u.RequestURI())
+	for i, v := range u.Query() {
+		fmt.Printf("Query    %v : %v\n", i, v[0])
+	}
 }

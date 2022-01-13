a16ca3ccca5843b24caabc30792c500586d92bc9
Ccl <ccl.love.miku@gmail.com>
2022-01-13 17:41:58 +0800

    water

--- basic/goroutine/argumentEval/main.go
+++ basic/goroutine/argumentEval/main.go
@@ -7,12 +7,14 @@ import (
 
 func main() {
 
-	fmt.Println(time.Now())
+	fmt.Printf("Start : %v\n", time.Now())
 	go func(t time.Time) {
 		time.Sleep(500 * time.Millisecond)
-		fmt.Println(t)
-		fmt.Println(time.Now())
+		fmt.Printf("Input time in goroutine : %v\n", t)
+		fmt.Printf("Goroutine now : %v\n", time.Now())
 	}(time.Now())
+
+	defer fmt.Printf("Time in defer : %v\n", time.Now()) // argument will passed to defer immediately
 
 	// arguments in go call will be evaluated immediately
 

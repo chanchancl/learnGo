5eec49739a7081a80280e5242417ac0f8b6db516
Ccl <ccl.love.miku@gmail.com>
2022-04-07 17:29:20 +0800

    water timer

--- basic/time/timeAfter/main.go
+++ basic/time/timeAfter/main.go
@@ -2,14 +2,84 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"sync"
 	"time"
 )
 
+var timer *time.Timer
+var lock sync.Mutex
+
+func SetTimer() {
+	lock.Lock()
+	defer lock.Unlock()
+	if timer == nil {
+		// Do some work
+		fmt.Println("Set timer successful")
+		timer = time.AfterFunc(time.Second, func() {
+			fmt.Println("Start to do work")
+			time.Sleep(time.Millisecond * 500)
+			fmt.Println("End to do work")
+			lock.Lock()
+			defer lock.Unlock()
+			fmt.Println("Timer expired and work done")
+			if timer == nil {
+				fmt.Println("Timer is nil when work done")
+			}
+			timer = nil
+		})
+	}
+}
+
+func CancelTimer() {
+	lock.Lock()
+	defer lock.Unlock()
+	if timer != nil {
+		fmt.Println("Success to cancel timer")
+		timer.Stop()
+		timer = nil
+	}
+}
+
 func main() {
-	fmt.Println(time.Now().After(time.Time{}))
+	fmt.Println("Test 1 : Just SetTimer")
+	fmt.Println()
+	SetTimer()
+	time.Sleep(time.Second * 2)
+	fmt.Println(strings.Repeat("*", 80))
 
-	// 看到了比较奇怪的代码，所以验证一下
-	fmt.Println(time.Now().Sub(time.Time{}).String())
+	fmt.Println("Test 2 : Multi SetTimer")
+	fmt.Println()
+	for i := 0; i < 100000; i++ {
+		go SetTimer()
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Println(strings.Repeat("*", 80))
 
-	// 结论是，代码真的很奇怪，没有任何作用
+	fmt.Println("Test 3 : Multi CancelTimer")
+	fmt.Println()
+	SetTimer()
+	for i := 0; i < 100000; i++ {
+		go CancelTimer()
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Println(strings.Repeat("*", 80))
+
+	fmt.Println("Test 4 : Cancel timer when it expired but doing work")
+	fmt.Println()
+	SetTimer()
+	time.Sleep(time.Second)
+	CancelTimer()
+	time.Sleep(time.Second)
+
+	fmt.Println(strings.Repeat("*", 80))
+
+	fmt.Println("Test 5 : Cancel timer just a little early than expired, to compare with test 4")
+	fmt.Println()
+	SetTimer()
+	time.Sleep(time.Millisecond * 990)
+	CancelTimer()
+	time.Sleep(time.Second)
+
+	fmt.Println(strings.Repeat("*", 80))
 }

c116fb01f6e28bbe8081e05d0e5c2fe0daed3d72
Ccl <ccl.love.miku@gmail.com>
2023-03-20 19:31:28 +0800

    water value

--- basic/json/onlyvalue/main.go
+++ basic/json/onlyvalue/main.go
@@ -5,10 +5,25 @@ import (
 	"fmt"
 )
 
+type body struct {
+	A string `json:"valueA"`
+	B string `json:"valueB"`
+}
+
 func main() {
 	v := uint8(5)
 
 	json.Unmarshal([]byte("abcd"), &v)
 
 	fmt.Println(v)
+
+	// json type
+	// null, string, float, bool, []array, {}dict
+	b := []*body{
+		{"1", "2"},
+		{"4", "5"},
+	}
+	result, _ := json.Marshal(b)
+
+	fmt.Println(string(result))
 }
